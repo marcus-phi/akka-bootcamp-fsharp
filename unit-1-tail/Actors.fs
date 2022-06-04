2189ffeeaf7e53c37bf74582d67b7e1e625b42b4
Marcus Phi <marcus@marcusphi.com>
2022-06-03 23:20:06 -0500

    lesson 1

--- unit-1-tail/Actors.fs
+++ unit-1-tail/Actors.fs
@@ -10,26 +10,29 @@ module Actors =
         | Continue
         | Message of string
         | Exit
-        
+
     let (|Message|Exit|) (str: string) =
         match str.ToLower() with
         | "exit" -> Exit
         | _ -> Message(str)
-        
+
     let consoleReadActor (consoleWriter: IActorRef) (mailbox: Actor<_>) message =
-        let line = Console.ReadLine ()
+        let line = Console.ReadLine()
+
         match line with
         | Exit -> mailbox.Context.System.Terminate() |> ignore
-        | Message(input) -> ()
-        
+        | Message (input) ->
+            consoleWriter <! input
+            mailbox.Self <! Continue
+
     let consoleWriterActor message =
         let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
-        
+
         let printInColor color message =
             Console.ForegroundColor <- color
             Console.WriteLine(message.ToString())
             Console.ResetColor()
-        
+
         match message.ToString().Length with
         | 0 -> printInColor ConsoleColor.DarkYellow "Please provide an input.\n"
         | Even -> printInColor ConsoleColor.Red "Your string had an even # of characters.\n"
--- unit-1-tail/Program.fs
+++ unit-1-tail/Program.fs
@@ -1,20 +1,35 @@
 ﻿open System
+open Akka.FSharp
+open unit_1_tail
 
 let printInstructions () =
     Console.WriteLine "Write whatever you want into the console!"
     Console.Write "Some lines will appear as"
     Console.ForegroundColor <- ConsoleColor.Red
     Console.Write " red"
-    Console.ResetColor ()
+    Console.ResetColor()
     Console.Write " and others will appear as"
     Console.ForegroundColor <- ConsoleColor.Green
     Console.Write " green! "
-    Console.ResetColor ()
-    Console.WriteLine ()
-    Console.WriteLine ()
+    Console.ResetColor()
+    Console.WriteLine()
+    Console.WriteLine()
     Console.WriteLine "Type 'exit' to quit this application at any time.\n"
-    
+
 [<EntryPoint>]
 let main argv =
     printInstructions ()
+
+    let myActorSystem =
+        System.create "MyActorSystem" (Configuration.load ())
+
+    let consoleWriterActor =
+        spawn myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor)
+
+    let consoleReaderActor =
+        spawn myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReadActor consoleWriterActor))
+
+    consoleReaderActor <! Actors.Start
+
+    myActorSystem.WhenTerminated.Wait()
     0
