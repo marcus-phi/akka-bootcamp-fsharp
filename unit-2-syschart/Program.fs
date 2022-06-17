c414e84a7e1e57dd7ada1806cae60d795dce78c3
Marcus Phi <marcus@marcusphi.com>
2022-06-17 11:32:58 -0500

    unit 2 start blank app

---
+++ unit-2-syschart/Program.fs
@@ -0,0 +1,49 @@
+﻿namespace unit_2_syschart
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Elmish
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Hosts
+
+type MainWindow() as this =
+    inherit HostWindow()
+    do
+        base.Title <- "System Metrics"
+        base.Width <- 684.0
+        base.Height <- 446.0
+        
+        let init () = ()
+        let update msg model =
+            model
+        let view model dispatch =
+            DockPanel.create [
+            ]
+                
+        Elmish.Program.mkSimple init update view
+        |> Program.withHost this
+        |> Program.run
+        
+type App() =
+    inherit Application()
+    
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
+        
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+    
+module Program =
+    
+    [<EntryPoint>]
+    let main args =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
