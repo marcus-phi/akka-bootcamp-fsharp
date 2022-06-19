08f5e3179a5e81211aaec08cc7365c26ff02097d
Marcus Phi <marcus@marcusphi.com>
2022-06-18 21:44:02 -0500

    unit 2 empty gui app

--- unit-2-syschart/Program.fs
+++ unit-2-syschart/Program.fs
@@ -1,9 +1,42 @@
-﻿namespace unit_2_syschart
+﻿namespace syschart
 
 open Avalonia
-  
-module Program =
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Hosts
+open Avalonia.Themes.Fluent
+
+[<AbstractClass; Sealed>]
+type Views =
     
+    static member main () =
+        Component (fun ctx ->
+            DockPanel.create [
+            ]
+        )
+    
+type MainWindow() =
+    inherit HostWindow()
+    do
+        base.Title <- "System Metrics"
+        base.Width <- 684
+        base.Height <- 446
+        base.Content <- Views.main ()
+        
+type App() =
+    inherit Application()
+    
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme(baseUri = null, Mode = FluentThemeMode.Dark))
+        
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
     [<EntryPoint>]
     let main args =
         AppBuilder
