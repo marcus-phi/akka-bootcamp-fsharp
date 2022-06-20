e0b08cc36ef25ba042619de7c59bb17b2b5c8ca5
Marcus Phi <marcus@marcusphi.com>
2022-06-20 00:29:16 -0500

    lesson 2 - add new random series

--- unit-2-syschart/Actors.fs
+++ unit-2-syschart/Actors.fs
@@ -1,21 +1,45 @@
 namespace syschart
 
+open System
+open Akka.FSharp.Actors
+open ScottPlot
 open ScottPlot.Avalonia
 open syschart.Types
 
 module Messages =
-    type InitializeChart = InitializeChart of plot: AvaPlot * initialSeries: Series
+    type ChartMessage =
+        | InitializeChart of plot: AvaPlot * initialSeries: Series
+        | AddSeries of plot: AvaPlot * series: Series
 
 open Messages
 
 module Actors =
-    let chartingActor message =
-        match message with
-        | InitializeChart (plot, series) ->
-            plot.Plot.Clear()
+    let chartingActor (mailbox: Actor<_>) =
+        let addScatter (plot: AvaPlot) (series: Series) =
+            let newPlottable =
+                plot.Plot.AddScatter(series.DataX, series.DataY, lineWidth = 2.f, markerSize = 0.f, label = series.Name)
 
-            plot.Plot.AddScatter(series.DataX, series.DataY, lineWidth = 2.f, markerSize = 0.f, label = series.Name)
-            |> ignore
+            plot.Refresh()
+            newPlottable
 
-            plot.Plot.Legend()
-            |> ignore
+        let rec charting (mapping: Map<string, Plottable.ScatterPlot>) =
+            actor {
+                let! message = mailbox.Receive()
+
+
+                let newMapping =
+                    match message with
+                    | InitializeChart (plot, series) ->
+                        plot.Plot.Clear()
+                        mapping.Add(series.Name, addScatter plot series)
+                    | AddSeries (plot, series) when
+                        not <| String.IsNullOrEmpty series.Name
+                        && not <| (mapping |> Map.containsKey series.Name)
+                        ->
+                        mapping.Add(series.Name, addScatter plot series)
+                    | _ -> mapping
+
+                return! charting newMapping
+            }
+
+        charting Map.empty<string, Plottable.ScatterPlot>
--- unit-2-syschart/Program.fs
+++ unit-2-syschart/Program.fs
@@ -8,44 +8,59 @@ open Avalonia.FuncUI
 open Avalonia.FuncUI.Builder
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Hosts
+open Avalonia.Layout
 open Avalonia.Themes.Fluent
 open ScottPlot.Avalonia
 open syschart.Messages
-
-type State =
-    | NotInitialized
-    | Initiliazed
 
 [<AbstractClass; Sealed>]
 type Views =
 
     static member main actorSystem =
         let chartActor =
-            spawn actorSystem "charting" (actorOf Actors.chartingActor)
+            spawn actorSystem "charting" Actors.chartingActor
 
         let series =
             ChartDataHelper.randomSeries "FakeSeries1" None
 
         Component (fun ctx ->
-            let appState = ctx.useState NotInitialized
+
             let plotOutlet =
                 ctx.useState (null, renderOnChange = false)
 
             ctx.useEffect (
-                handler = (fun () ->
-                    match appState.Current with
-                    | NotInitialized ->
-                        appState.Set Initiliazed
-                        chartActor <! InitializeChart (plotOutlet.Current, series)
-                    | _ -> ()
-                ),
+                handler =
+                    (fun () ->
+                        chartActor
+                        <! InitializeChart (plotOutlet.Current, series)),
                 triggers = [ EffectTrigger.AfterInit ],
                 identity = "initialize"
             )
 
-            DockPanel.create [ DockPanel.lastChildFill true
-                               DockPanel.children [ ViewBuilder.Create<AvaPlot>([])
-                                                    |> View.withOutlet plotOutlet.Set ] ])
+            DockPanel.create [
+                DockPanel.lastChildFill true
+                DockPanel.children [
+                    DockPanel.create [
+                        DockPanel.dock Dock.Right
+                        DockPanel.children [
+                            Button.create [
+                                Button.dock Dock.Bottom
+                                Button.verticalAlignment VerticalAlignment.Bottom
+                                Button.margin 5
+                                Button.padding (10., 5.)
+                                Button.content "Add Series"
+                                Button.onClick (fun _ ->
+                                    let newSeriesName = $"FakeSeries {plotOutlet.Current.Plot.GetPlottables().Length + 1}"
+                                    let newSeries = ChartDataHelper.randomSeries newSeriesName None
+                                    chartActor <! AddSeries (plotOutlet.Current, newSeries)
+                                )
+                            ]
+                        ]
+                    ]
+                    ViewBuilder.Create<AvaPlot>([])
+                    |> View.withOutlet plotOutlet.Set
+                ]
+            ])
 
 type MainWindow() =
     inherit HostWindow()
